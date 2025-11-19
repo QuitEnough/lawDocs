9880c31ec107319150d8585639a5b8b6aeeca241
TestingMyself0 <workspace01001@gmail.com>
2025-11-19 15:11:33 +0300

    removed unused statements

--- file-storage-service/src/main/java/com/yana/filestorage/exception/BaseExceptionHandler.java
+++ file-storage-service/src/main/java/com/yana/filestorage/exception/BaseExceptionHandler.java
@@ -3,7 +3,6 @@ package com.yana.filestorage.exception;
 import com.yana.filestorage.api.dto.ErrorResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
