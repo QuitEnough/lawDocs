cdbc16f0c53af891036cd3d8f4fcdb36995ab0cb
TestingMyself0 <workspace01001@gmail.com>
2025-12-02 09:23:58 +0300

    v1

---
+++ ai/src/main/java/com/yana/ai/AiApplication.java
@@ -0,0 +1,23 @@
+package com.yana.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.ai.chat.client.ChatClient;
+
+@SpringBootApplication
+public class AiApplication {
+
+    @Bean
+    public ChatClient chatClient(ChatClient.Builder builder) {
+        return builder.build();
+    }
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(AiApplication.class, args);
+        ChatClient chatClient = context.getBean(ChatClient.class);
+        System.out.println(chatClient.prompt().user("Дай первую строчку богемской рапсодии").call().content());
+    }
+
+}
